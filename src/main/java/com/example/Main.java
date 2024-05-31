8a7bb53d684090c1da8c60ab80381abb1bb0d00c
bagheera <r.boeran@gmail.com>
2024-05-31 10:29:44 +0700

    maven dan ci/cd

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+    
+    public sataic double calculateDiscount(double price, double discountPercentage) {
+        return (price * discountPercentage) / 100;
+    }
+
+    public static double calculateTotalPrice(double price, double discount) {
+        return price - discount;
+    }
+    public static void main(String[] args) {
+        double price = 100.0;
+        double discountPercentage = 10.0;
+
+        double discount = calculateDiscount(price, discountPercentage);
+        double totalPrice = calculateTotalPrice(price, discount);
+
+        System.out.println("Discount: $" + discount);
+        System.out.println("Total Price: $" + totalPrice);
+        System.out.println("Hello world!");
+    }
+}
